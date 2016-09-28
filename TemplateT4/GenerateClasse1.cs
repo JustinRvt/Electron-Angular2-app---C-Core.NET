787422154bc4a504d9925e78adf61dde95c1ac0d
U-ACA\jravat <jravat@EXO-257.exo.com>
2016-09-28 12:29:01 +0200

    rename ok

--- TemplateT4/GenerateClasse1.cs
+++ TemplateT4/GenerateClasse1.cs
@@ -19,9 +19,9 @@
 
 // Generated helper templates
 // Generated items
-// WS_Hotline.SiteWeb\app\DTO\User\CritereUserDTO.ts
-// WS_Hotline.SiteWeb\app\DTO\User\UserDTO.ts
-// WS_Hotline.SiteWeb\Controllers\UserController.cs
+// WS_Hotline.ServicesWeb\app\DTO\User\CritereUserDTO.ts
+// WS_Hotline.ServicesWeb\app\DTO\User\UserDTO.ts
+// WS_Hotline.ServicesWeb\Controllers\UserController.cs
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\UnitTestDomain\Entities\User\UserMetierTest.cs
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\WS_Hotline.DomainLibrary\Metier\User\UserMetier.cs
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\UnitTestDAL\Entities\User\UserDALTest.cs
@@ -30,9 +30,9 @@
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\WS_Hotline.DTOLibrary\Exception\UserException\UserException.cs
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\WS_Hotline.DTOLibrary\Entities\User\CritereUserDTO.cs
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\WS_Hotline.DTOLibrary\Entities\User\UserDTO.cs
-// WS_Hotline.SiteWeb\app\DTO\Softlog\CritereSoftlogDTO.ts
-// WS_Hotline.SiteWeb\app\DTO\Softlog\SoftlogDTO.ts
-// WS_Hotline.SiteWeb\Controllers\SoftlogController.cs
+// WS_Hotline.ServicesWeb\app\DTO\Softlog\CritereSoftlogDTO.ts
+// WS_Hotline.ServicesWeb\app\DTO\Softlog\SoftlogDTO.ts
+// WS_Hotline.ServicesWeb\Controllers\SoftlogController.cs
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\UnitTestDomain\Entities\Softlog\SoftlogMetierTest.cs
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\WS_Hotline.DomainLibrary\Metier\Softlog\SoftlogMetier.cs
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\UnitTestDAL\Entities\Softlog\SoftlogDALTest.cs
@@ -41,9 +41,9 @@
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\WS_Hotline.DTOLibrary\Exception\SoftlogException\SoftlogException.cs
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\WS_Hotline.DTOLibrary\Entities\Softlog\CritereSoftlogDTO.cs
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\WS_Hotline.DTOLibrary\Entities\Softlog\SoftlogDTO.cs
-// WS_Hotline.SiteWeb\app\DTO\Ecriture\CritereEcritureDTO.ts
-// WS_Hotline.SiteWeb\app\DTO\Ecriture\EcritureDTO.ts
-// WS_Hotline.SiteWeb\Controllers\EcritureController.cs
+// WS_Hotline.ServicesWeb\app\DTO\Ecriture\CritereEcritureDTO.ts
+// WS_Hotline.ServicesWeb\app\DTO\Ecriture\EcritureDTO.ts
+// WS_Hotline.ServicesWeb\Controllers\EcritureController.cs
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\UnitTestDomain\Entities\Ecriture\EcritureMetierTest.cs
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\WS_Hotline.DomainLibrary\Metier\Ecriture\EcritureMetier.cs
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\UnitTestDAL\Entities\Ecriture\EcritureDALTest.cs
@@ -52,9 +52,9 @@
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\WS_Hotline.DTOLibrary\Exception\EcritureException\EcritureException.cs
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\WS_Hotline.DTOLibrary\Entities\Ecriture\CritereEcritureDTO.cs
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\WS_Hotline.DTOLibrary\Entities\Ecriture\EcritureDTO.cs
-// WS_Hotline.SiteWeb\app\DTO\Dossier\CritereDossierDTO.ts
-// WS_Hotline.SiteWeb\app\DTO\Dossier\DossierDTO.ts
-// WS_Hotline.SiteWeb\Controllers\DossierController.cs
+// WS_Hotline.ServicesWeb\app\DTO\Dossier\CritereDossierDTO.ts
+// WS_Hotline.ServicesWeb\app\DTO\Dossier\DossierDTO.ts
+// WS_Hotline.ServicesWeb\Controllers\DossierController.cs
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\UnitTestDomain\Entities\Dossier\DossierMetierTest.cs
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\WS_Hotline.DomainLibrary\Metier\Dossier\DossierMetier.cs
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\UnitTestDAL\Entities\Dossier\DossierDALTest.cs
@@ -63,9 +63,9 @@
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\WS_Hotline.DTOLibrary\Exception\DossierException\DossierException.cs
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\WS_Hotline.DTOLibrary\Entities\Dossier\CritereDossierDTO.cs
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\WS_Hotline.DTOLibrary\Entities\Dossier\DossierDTO.cs
-// WS_Hotline.SiteWeb\app\DTO\Compte\CritereCompteDTO.ts
-// WS_Hotline.SiteWeb\app\DTO\Compte\CompteDTO.ts
-// WS_Hotline.SiteWeb\Controllers\CompteController.cs
+// WS_Hotline.ServicesWeb\app\DTO\Compte\CritereCompteDTO.ts
+// WS_Hotline.ServicesWeb\app\DTO\Compte\CompteDTO.ts
+// WS_Hotline.ServicesWeb\Controllers\CompteController.cs
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\UnitTestDomain\Entities\Compte\CompteMetierTest.cs
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\WS_Hotline.DomainLibrary\Metier\Compte\CompteMetier.cs
 // C:\Tortoise Local\Dev EN cours\Projet Demo Dev\UnitTestDAL\Entities\Compte\CompteDALTest.cs
