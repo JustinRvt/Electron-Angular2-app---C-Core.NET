f07da59fe98273bea848bd00dd1d4c999998d294
U-ACA\jravat <jravat@EXO-257.exo.com>
2016-09-28 11:25:05 +0200

    bfr renaming webservices

--- WS_Hotline.WPF/Properties/Resources.Designer.cs
+++ WS_Hotline.WPF/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Demo.WPF.Properties {
+namespace WS_Hotline.WPF.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Demo.WPF.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Demo.WPF.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WS_Hotline.WPF.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
