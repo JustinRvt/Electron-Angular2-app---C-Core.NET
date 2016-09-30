9c3b5d0f4ae3aee07b1ae481f211589f87d83d15
U-ACA\jravat <jravat@EXO-257.exo.com>
2016-09-30 09:49:38 +0200

    switching folders

---
+++ WS_Hotline.DomainLibrary/Metier/Customer/CustomerMetier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WS_Hotline.DTOLibrary.Entities.Customer;
+
+namespace WS_Hotline.DomainLibrary.Metier.Customer
+{
+    /// <summary>
+    /// Classe métier de Name
+    /// </summary>
+    /// <remark>[jravat] - [30092016] - Généré par snippet v1.0</remark>
+    public class CustomerMetier : BaseMetier<CustomerDAL, CustomerDTO, CritereCustomerDTO, CustomerException>
+    {
+
+    }
+}
